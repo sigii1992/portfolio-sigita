68a327c14806e93c7e1a3f284aab0a06182eb1c9
Sigii1992 <sawlete@gmail.com>
2022-07-12 16:48:25 +0100

    Create navbar, add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import Logo from '../assets/sz.png'
+
+const NavBar = () => {
+  return (
+    <div className="bg-gradient-to-r from-rose-500 via-red-400 to-red-500 fixed w-full h-[80px] flex justify-between items-center px-4 text-white">
+        <div>
+            <img src={Logo} style={{width: '110px'}}></img>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar;
