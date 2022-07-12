4c130a8e8446789ec58721d8f9723bdec859b0d5
Sigii1992 <sawlete@gmail.com>
2022-07-12 17:49:02 +0100

    Add Navbar meny items and style it

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,16 @@ const NavBar = () => {
   return (
     <div className="bg-gradient-to-r from-rose-500 via-red-400 to-red-500 fixed w-full h-[80px] flex justify-between items-center px-4 text-white">
         <div>
-            <img src={Logo} style={{width: '110px'}}></img>
+            <img src={Logo} alt='Logo' style={{width: '110px'}}></img>
+        </div>
+        <div>
+            <ul className='flex'>
+              <li>Home</li>
+              <li>About Me</li>
+              <li>Skills</li>
+              <li>Projects</li>
+              <li>Contact Me</li>
+            </ul>
         </div>
     </div>
   )
