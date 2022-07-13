0a7495b1ed819c7bfb47bb50fb2411e072b3cb6b
Sigii1992 <sawlete@gmail.com>
2022-07-13 13:38:49 +0100

    Add shadow to the navbar and navbar text, add hamburger menu than the screen is smaller than 768px"

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,35 @@
-import Logo from '../assets/sz.png'
+import Logo from '../assets/sz.png';
+import { FaBars } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <div className="bg-gradient-to-r from-rose-500 via-red-400 to-red-500 fixed w-full h-[80px] flex justify-between items-center px-4 text-white">
+    <div className="bg-gradient-to-r from-rose-500 via-red-400 to-red-500 fixed w-full h-[80px] flex justify-between items-center px-4 text-white shadow-xl">
         <div>
             <img src={Logo} alt='Logo' style={{width: '110px'}}></img>
         </div>
-        <div>
-            <ul className='flex'>
-              <li>Home</li>
-              <li>About Me</li>
-              <li>Skills</li>
-              <li>Projects</li>
-              <li>Contact Me</li>
-            </ul>
+
+        {/*menu: anything below medium it will hide*/}
+          <ul className='hidden md:flex'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+          </ul>
+
+        {/*hamburger: anything over medium (768px) it will hide*/}
+        <div className='md:hidden'>
+            <FaBars />
         </div>
+
+        {/*Mobile menu*/}
+          <ul className='hidden'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+          </ul>
     </div>
   )
 }
