33d9c7708a42ced016dc6f3d9fb613030cf59b77
Sigii1992 <sawlete@gmail.com>
2022-07-13 14:46:38 +0100

    Add hamburger menu items, style it, make it appear onClick

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import Logo from '../assets/sz.png';
 import { FaBars } from 'react-icons/fa';
 
 const NavBar = () => {
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
+
+  const handleClick = () => setHamburgerMenu(!hamburgerMenu);
+
   return (
     <div className="bg-gradient-to-r from-rose-500 via-red-400 to-red-500 fixed w-full h-[80px] flex justify-between items-center px-4 text-white shadow-xl">
         <div>
@@ -18,17 +23,17 @@ const NavBar = () => {
           </ul>
 
         {/*hamburger: anything over medium (768px) it will hide*/}
-        <div className='md:hidden'>
-            <FaBars />
+        <div onClick={handleClick} className='md:hidden z-10 hover:text-red-700 cursor-pointer drop-shadow-lg'>
+            <FaBars size={38}/>
         </div>
 
         {/*Mobile menu*/}
-          <ul className='hidden'>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact Me</li>
+          <ul className={hamburgerMenu ? 'absolute top-0 left-0 w-full h-screen bg-gradient-to-r from-rose-500 via-red-400 to-red-500 flex flex-col justify-center items-center' : 'hidden'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About Me</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Projects</li>
+            <li className='py-6 text-4xl'>Contact Me</li>
           </ul>
     </div>
   )
