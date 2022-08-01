211d2e66d5fff3ba56360d05e69317ebcaf73bec
Sigii1992 <sawlete@gmail.com>
2022-08-01 13:43:33 +0100

    Create new component Contact, add text and style

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+
+
+const Contact = () => {
+  return (
+    <div name='contact' className="w-full min-h-screen flex justify-center items-center bg-gradient-to-r from-rose-100 to-teal-100 font-[Kanit] text-[#d18282]">
+        <form className="max-w-[600px] flex flex-col w-full px-4 sm:px-12 pb-20">
+            <div>
+              <p className="text-4xl font-bold inline border-b-4 border-red-700 text-red-500">Contact me</p>
+              <p className="pt-10">Submit the form below or send me an email - sigita.zeb92@gmail.com</p>
+            </div> 
+        </form>
+    </div>
+  )
+}
+
+export default Contact
